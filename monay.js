91862299d688e687c77094526db96b373da5dd4e
sintendo <espeon.78s@gmail.com>
2019-06-24 14:31:59 -0400

    testing reactions

--- monay.js
+++ monay.js
@@ -3,6 +3,8 @@ run: function(message) {
 	if (message.content == "!board-test") {
 		var board = ["1111111\n","1111111\n","1111111\n","1111111\n","1111111\n","1111111"];
 		message.channel.send(board[0].replace(/1/g, '⚪') + board[1].replace(/1/g, '⚪') + board[2].replace(/1/g, '⚪') + board[3].replace(/1/g, '⚪') + board[4].replace(/1/g, '⚪') + board[5].replace(/1/g, '⚪'));
+		
+		message.react('1️⃣').then(() => message.react('2️⃣'));
 	}
 }
 }
