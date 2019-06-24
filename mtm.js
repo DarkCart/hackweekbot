8d79928d2044d8dd82060d83a24e6c1326a9582a
MisterTalkingMachine <52059616+MisterTalkingMachine@users.noreply.github.com>
2019-06-24 18:23:53 -0400

    Update mtm.js

--- mtm.js
+++ mtm.js
@@ -1,4 +1,17 @@
 module.exports = {
 run: function(message) {
+	if (message.content == "ping") {
+        message.channel.send("No, fuck you.");
+    }
+	if (message.content === 'pfp') {
+        message.channel.send(message.author.avatarURL);
+	}
+	if (message.content === 'g') {
+		var filter = true;
+		do{
+			const attachment = new Discord.ReactionCollector(message,filter,options).prototype.size;
+		}while(attachment != 0);
+		message.channel.send(attachment);
+	}
 }
 }
