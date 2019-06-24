359cf2d7fb64e673d0bb4d2e5317644df7471e6a
John <minecart94@gmail.com>
2019-06-24 12:46:51 -0500

    adding video support

--- sgt.js
+++ sgt.js
@@ -1,4 +1,5 @@
 var i = 0;
+var intervalTimer;
 
 module.exports = {
 run: function(message) {
@@ -7,11 +8,38 @@ run: function(message) {
 	}
 	if (message.content === "!test") {
 		message.channel.send("test").then((msg)=>{
-			setInterval(function() {
+			intervalTimer = setInterval(function() {
 				msg.edit("test" + i);
 				i++;
-			}, 500);
+			}, 1500);
 		});
 	}
+
+	if (message.content === "!stop") {
+		message.channel.send("Stopping.");
+		clearInterval(intervalTimer);
+	}
+
+	if (message.content == "!load") {
+		var filename = message.content.split(" ")[1];
+		splitVideo(filename);
+		message.channel.send("Loaded file '" + filename + "' and split into frames.");
+	}
 }
+}
+
+function splitVideo(filename) {
+     try {
+        var process = new ffmpeg(filename);
+        process.then(function (video) {
+            video.fnExtractFrameToJPG("frames", {
+                every_n_frames : 1
+            }, myCallbackFunction())
+        }, function (err) {
+            console.log('Error: ' + err);
+        });
+    } catch (e) {
+        console.log(e.code);
+        console.log(e.msg);
+    }
 }
