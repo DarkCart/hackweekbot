f4b01df5d49c4ceab543836ba324b4cd6025e8e2
sintendo <espeon.78s@gmail.com>
2019-06-24 14:39:26 -0400

    rt3: return of the emoji

--- monay.js
+++ monay.js
@@ -4,7 +4,7 @@ run: function(message) {
 		var board = ["1111111\n","1111111\n","1111111\n","1111111\n","1111111\n","1111111"];
 		message.channel.send(board[0].replace(/1/g, '⚪') + board[1].replace(/1/g, '⚪') + board[2].replace(/1/g, '⚪') + board[3].replace(/1/g, '⚪') + board[4].replace(/1/g, '⚪') + board[5].replace(/1/g, '⚪'));
 		
-		message.react(':one:').then(() => message.react(':two:'));
+		message.react('🇦').then(() => message.react('🇧'));
 	}
 }
 }
