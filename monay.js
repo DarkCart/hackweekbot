beba5a0a0d37a8ff0589554e19370f4fd3a827f1
sintendo <espeon.78s@gmail.com>
2019-06-24 15:39:09 -0400

    reaction collection test

--- monay.js
+++ monay.js
@@ -1,17 +1,28 @@
 module.exports = {
 run: function(message) {
-	if (message.content == "!board-test") {
+	if (message.content == "!connectfour") {
 		var board = ["1111111\n","1111111\n","1111111\n","1111111\n","1111111\n","1111111"];
-		message.channel.send(board[0].replace(/1/g, '⚪') + board[1].replace(/1/g, '⚪') + board[2].replace(/1/g, '⚪') + board[3].replace(/1/g, '⚪') + board[4].replace(/1/g, '⚪') + board[5].replace(/1/g, '⚪'))
-			.then((msg) => {
-				msg.react('🇦')
-					.then(() => msg.react('🇧'))
-					.then(() => msg.react('🇨'))
-					.then(() => msg.react('🇩'))
-					.then(() => msg.react('🇪'))
-					.then(() => msg.react('🇫'))
-					.then(() => msg.react('🇬'));
-			});
+		//posting board
+		message.channel.send(board[0].replace(/1/g, '⚪') + board[1].replace(/1/g, '⚪') + board[2].replace(/1/g, '⚪') + board[3].replace(/1/g, '⚪') + board[4].replace(/1/g, '⚪') + board[5].replace(/1/g, '⚪')).then((msg) => {
+			//reacting with columns
+			msg.react('🇦')
+				.then(() => msg.react('🇧'))
+				.then(() => msg.react('🇨'))
+				.then(() => msg.react('🇩'))
+				.then(() => msg.react('🇪'))
+				.then(() => msg.react('🇫'))
+				.then(() => msg.react('🇬'));
+		});
+		//awaiting reactions
+		var filter = (reaction) => {
+			return ['🇦', '🇧', '🇨', '🇩', '🇪', '🇫', '🇬'].includes(reaction.emoji.name) && user.id === message.author.id;
+		};
+		
+		message.awaitReactions(filter, {time: 60000})
+			.then(collected => {
+				message.channel.send (collected.size + " reactions collected");
+			})
+			.catch(console.error);
 	}
 }
 }
