a843e7d1566c9d806630559b00cdfe61aeb72243
John <minecart94@gmail.com>
2019-06-24 12:30:31 -0500

    add editing test

--- sgt.js
+++ sgt.js
@@ -3,5 +3,10 @@ run: function(message) {
 	if (message.content === "!ping") {
 		message.channel.send("pong");
 	}
+	if (message.content === "!test") {
+		message.content.send("test").then((msg)=>{
+			msg.edit("test2");
+		});
+	}
 }
 }
