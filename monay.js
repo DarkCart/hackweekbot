1852b9e78657b76490ebd0c04621639e784159d9
sintendo <espeon.78s@gmail.com>
2019-06-24 14:24:22 -0400

    Update monay.js

--- monay.js
+++ monay.js
@@ -2,7 +2,7 @@ module.exports = {
 run: function(message) {
 	if (message.content == "!board-test") {
 		var board = ["1111111\n","1111111\n","1111111\n","1111111\n","1111111\n","1111111"];
-		message.channel.send(board[0].replace(1, '⚪') + board[1].replace(1, '⚪') + board[2].replace(1, '⚪') + board[3].replace(1, '⚪') + board[4].replace(1, '⚪') + board[5].replace(1, '⚪'));
+		message.channel.send(board[0].replace(/1/g, '⚪') + board[/1/g].replace(/1/g, '⚪') + board[2].replace(/1/g, '⚪') + board[3].replace(/1/g, '⚪') + board[4].replace(/1/g, '⚪') + board[5].replace(/1/g, '⚪'));
 	}
 }
 }
